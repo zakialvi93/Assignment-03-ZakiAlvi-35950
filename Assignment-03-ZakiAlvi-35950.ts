a79cbd695c05e42538589dba3fdb44af049b4fbd
Muhammad Zaki Alvi <zakialvi93@gmail.com>
2023-02-26 16:58:19 +0500

    passed arguments

--- Assignment-03-ZakiAlvi-35950.ts
+++ Assignment-03-ZakiAlvi-35950.ts
@@ -1,19 +1,19 @@
 abstract class Attendance //Abstraction
 {
-    abstract CheckAttendance(): void; //Abstraction
+    abstract CheckAttendance(s: string): void; //Abstraction
 }
 
 interface InterviewOfEmployee //Interface Segregation Principle
 {
-    Interview(): void;
+    Interview(s: string): void;
 }
 
 
 class DevOps implements InterviewOfEmployee //Interface Segregation Principle
 {
-    Interview():void //Interface Segregation Principle
+    Interview(s: string):void //Interface Segregation Principle
     {
-        console.log("One interview is conducted by DevOps Engineer");
+        console.log(s);
     }
 
     private Development(): void //Encapsulation and Single Responsibility Principle
@@ -29,9 +29,9 @@ class DevOps implements InterviewOfEmployee //Interface Segregation Principle
 
 class Accounts extends Attendance //Inheritance and Open/Closed Principle
 {
-    CheckAttendance(): void //Abstraction
+    CheckAttendance(s: string): void //Abstraction
     {
-        console.log("Attendance is determined by Accounts Department");
+        console.log(s);
     }
 
     private Audit(): void //Encapsulation and Single Responsibility Principle
@@ -42,14 +42,14 @@ class Accounts extends Attendance //Inheritance and Open/Closed Principle
 
 class HumanResource extends Attendance implements InterviewOfEmployee 
 {
-    Interview():void //Interface Segregation Principle
+    Interview(s: string):void //Interface Segregation Principle
     {
-        console.log("Another interview is conducted by HR");
+        console.log(s);
     }
 
-    CheckAttendance(): void //Abstraction
+    CheckAttendance(s: string): void //Abstraction
     {
-        console.log("Attendance is also determined by HR Department");
+        console.log(s);
     }
 
     private MaintainRecords(): void //Encapsulation and Single Responsibility Principle
@@ -64,9 +64,9 @@ var Hr = new HumanResource(); //Dependency Inversion Principle
 
 console.log("Some Common Properties in different classes are: ");
 console.log("Property of Interview: ");
-Dev.Interview(); //Polymorphism
-Hr.Interview();  //Polymorphism
+Dev.Interview("One interview is conducted by DevOps Engineer"); //Polymorphism
+Hr.Interview("Another interview is conducted by HR");  //Polymorphism
 console.log("Property of Checking Attendance: ");
-Acc.CheckAttendance(); //Polymorphism
-Hr.CheckAttendance(); //Polymorphism
+Acc.CheckAttendance("Attendance is determined by Accounts Department"); //Polymorphism
+Hr.CheckAttendance("Attendance is also determined by HR Department"); //Polymorphism
 
